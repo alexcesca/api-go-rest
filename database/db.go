d33aee059cb78c493e558114e08163e4d1cbfd07
Alex Cesca <alexcesca@gmail.com>
2022-02-11 00:51:52 -0300

    Criando banco local e acesso ao mesmo.

---
+++ database/db.go
@@ -0,0 +1,22 @@
+package database
+
+import (
+	"log"
+
+	_ "github.com/lib/pq"
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+)
+
+var (
+	DB  *gorm.DB
+	err error
+)
+
+func ConectaBanco() {
+	connStr := "user=postgres dbname=personalidades password=.A9l13ed0m7., host=localhost sslmode=disable"
+	DB, err = gorm.Open(postgres.Open(connStr))
+	if err != nil {
+		log.Panic("Erro ao conectar o banco de dados")
+	}
+}
--- main.go
+++ main.go
@@ -3,10 +3,12 @@ package main
 import (
 	"fmt"
 	"go-rest-api/controllers/models"
+	"go-rest-api/database"
 	"go-rest-api/routes"
 )
 
 func main() {
+	database.ConectaBanco()
 	models.Personalidades = []models.Personalidade{
 		{Id: 1, Nome: "nome 1", Historia: "hostiria 1"},
 		{Id: 2, Nome: "nome 2", Historia: "hostiria 2"},
